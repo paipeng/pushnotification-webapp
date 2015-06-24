8c55dfabb7a494d69b871258694aae9ba87994b3
Pai Peng <sipaipv6@gmail.com>
2015-06-24 06:32:57 +0200

    init angular project with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pushnotificationWebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pushnotificationWebappApp
+ */
+angular.module('pushnotificationWebappApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
